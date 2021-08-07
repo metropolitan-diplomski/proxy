3e10fe4b7f8d1d54af52f138f872019cb3a8ecb6
Nemanja Petrovic <nemanja@MacBooks-MacBook-Pro.local>
2021-08-07 13:20:02 +0200

    first commit

---
+++ src/test/java/com/met/proxy/ProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.met.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
