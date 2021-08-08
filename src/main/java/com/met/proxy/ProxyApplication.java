8e4032605f8d0f6fb52c9baf942e72530c3777fc
nemanja <nemanjabosnjane@gmail.com>
2021-08-08 12:33:48 +0200

    Configuration

--- src/main/java/com/met/proxy/ProxyApplication.java
+++ src/main/java/com/met/proxy/ProxyApplication.java
@@ -1,5 +1,6 @@
 package com.met.proxy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -7,7 +8,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class ProxyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
